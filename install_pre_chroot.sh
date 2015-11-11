e4553e85f9c0e4069acb1db39c90a2b301c1d2f2
ygoreus <ygoreus@gmail.com>
2015-11-11 16:50:46 -0500

    Added host selector

--- install_pre_chroot.sh
+++ install_pre_chroot.sh
@@ -1,15 +1,31 @@
 #!/usr/bin/env sh
-host=""
+##-- vim: set ai et ts=4 sw=4 ft=zsh syn=zsh :
+##-- host selector
+case $1 in
+    "laptop"|"")
+        host="Charon"
+    ;;
+    "desktop")
+        host="Kerberos"
+    ;;
+    *)
+        host="Charon"
+    ;;
+esac
 
 ##-- prepare disks for installation
 echo -e "\n >>> Erasing disk sda..."
 sgdisk --zap-all /dev/sda
+
 echo -e "\n >>> Partition the disk sda..."
 parted -a optimal /dev/sda mklabel gpt mkpart primary 2MiB 1024MiB mkpart primary 1024MiB 100% name 1 bootfs name 2 rootfs set 2 lvm on
+
 echo -e "\n >>> encrypt rootfs partition..."
 cryptsetup --verbose --cipher=aes-xts-plain64 --key-size=512 --hash=sha512 --iter-time=5000 --use-random --verify-passphrase luksFormat /dev/sda2
+
 echo -e "\n >>> Opening the encrypted partition..."
 cryptsetup luksOpen /dev/sda2 lvm
+
 echo -e "\n >>> Setting up logical Volume (lvm)..."
 pvcreate /dev/mapper/lvm
 vgcreate ${host} /dev/mapper/lvm
@@ -17,11 +33,13 @@ lvcreate -L 16G ${host} -n swapvol
 lvcreate -L 100G ${host} -n rootvol
 lvcreate -l +100%FREE ${host} -n homevol
 vgchange -ay
+
 echo -e "\n >>> Formatting the file systems..."
 mkfs.ext4 -L BOOTFS /dev/sda1
 mkfs.ext4 -L ROOTFS /dev/mapper/lvm/${host}-rootvol
 mkfs.ext4 -L HOMEFS /dev/mapper/lvm/${host}-homevol
 mkswap /dev/mapper/lvm/${host}-swapvol
+
 echo -e "\n >>> mount file systems to mount points..."
 mount /dev/mapper/lvm/${host}-rootvol /mnt
 mkdir -p /mnt/home /mnt/boot
